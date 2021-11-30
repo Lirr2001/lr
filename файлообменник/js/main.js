2a255dd1c52b2dab90e461b0e391f9688dd5bf26
Lirr2001 <91262601+Lirr2001@users.noreply.github.com>
2021-11-30 09:52:26 +0300

    Delete файлообменник directory

--- файлообменник/js/main.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('.openWind');
-let overlay = document.getElementById('zatemnenie');
-button.addEventListener('click', function () {
-  console.log('click');
-  overlay.style.display = '';
-});
